33cc2c6e1f84179a90ecfeaff55efdd96b5d2929
sexnine <25860388+sexnine@users.noreply.github.com>
2022-05-01 17:53:02 +1000

    Style upload button

--- frontend/src/components/UploadBtn.vue
+++ frontend/src/components/UploadBtn.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    <input ref="inputEl" type="file" class="w-min bg-red-500" />
+    <input
+      ref="inputEl"
+      type="file"
+      class="w-64 rounded-md bg-gray-800 file:rounded-l-md file:border-0 file:bg-red-500 file:text-white"
+      @change="filesChanged"
+    />
   </div>
-  <button @click="upload">Upload</button>
+  <button
+    v-if="showUploadBtn"
+    class="rounded-md bg-red-500 px-2"
+    @click="upload"
+  >
+    Upload
+  </button>
 </template>
 
 <script setup lang="ts">
@@ -11,16 +22,21 @@
 
   const inputEl = ref<HTMLInputElement>(null);
 
+  const showUploadBtn = ref(false);
+
+  const filesChanged = () =>
+    (showUploadBtn.value = inputEl.value.files?.length > 0);
+
   const upload = () => {
     let files = inputEl.value.files;
-    console.log(files);
-    console.log(files[0]);
+
+    if (!files) return;
 
     let formData = new FormData();
-    formData.append("options", JSON.stringify({path: props.path}))
-    
+    formData.append("options", JSON.stringify({ path: props.path }));
+
     for (let i = 0; i < files?.length; i++) {
-      formData.append("file", files[i])
+      formData.append("file", files[i]);
     }
 
     axios
@@ -33,6 +49,6 @@
   };
 
   const props = defineProps<{
-    path?: string | null
+    path?: string | null;
   }>();
 </script>
