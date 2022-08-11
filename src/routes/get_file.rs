a3d18ad961143f95d7bfa7160d3867fa1eef02ef
sexnine <25860388+sexnine@users.noreply.github.com>
2022-08-11 16:20:52 +1000

    Improve downloading files

--- src/routes/get_file.rs
+++ src/routes/get_file.rs
@@ -1,19 +1,23 @@
 use std::path::Path;
 
-use actix_codec::Framed;
-
-use actix_web::{get, web, HttpResponse, Responder};
-
-use tokio::fs;
+use actix_files::NamedFile;
+use actix_web::{
+    get,
+    http::header::{ContentDisposition, DispositionParam, DispositionType},
+    web, HttpRequest, HttpResponse, Responder,
+};
 
 use crate::{
-    codec,
     models::{AppState, ErrRes, GetFileReq},
     util,
 };
 
 #[get("/get_file")]
-pub async fn get_file(query: web::Query<GetFileReq>, data: web::Data<AppState>) -> impl Responder {
+pub async fn get_file(
+    req: HttpRequest,
+    query: web::Query<GetFileReq>,
+    data: web::Data<AppState>,
+) -> impl Responder {
     let path = Path::new(&query.path);
     if !util::check_path(&path, &data) {
         return HttpResponse::BadRequest()
@@ -26,7 +30,7 @@ pub async fn get_file(query: web::Query<GetFileReq>, data: web::Data<AppState>) 
             .json(ErrRes::from_static("notFileErr", "Path is not a file"));
     }
 
-    let file = match fs::File::open(path).await {
+    let file = match NamedFile::open_async(path).await {
         Ok(i) => i,
         _ => {
             return HttpResponse::BadRequest().json(ErrRes::from_static(
@@ -36,22 +40,16 @@ pub async fn get_file(query: web::Query<GetFileReq>, data: web::Data<AppState>) 
         }
     };
 
-    let file_name = path
-        .file_name()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .to_string()
-        .replace("\"", "%22");
-    let stream = Framed::new(file, codec::WebBytesCodec {});
+    let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
 
-    return HttpResponse::Ok()
-        .insert_header((
-            "Content-Disposition",
-            format!(
-                "{}filename=\"{file_name}\"",
-                if *view_file { "" } else { "attachment; " }
-            ),
-        ))
-        .streaming(stream);
+    let file = file.set_content_disposition(ContentDisposition {
+        disposition: if *view_file {
+            DispositionType::Inline
+        } else {
+            DispositionType::Attachment
+        },
+        parameters: vec![DispositionParam::Filename(file_name)],
+    });
+
+    file.into_response(&req)
 }
