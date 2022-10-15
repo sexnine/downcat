2d0b2b8fdbc6ea63c15b24644d6a11be4f3da620
sexnine <25860388+sexnine@users.noreply.github.com>
2022-10-15 20:00:44 +1100

    Update ESLint + prettier config, update ESLint deps

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "@vue/typescript/recommended",
+    "prettier",
+    // "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  plugins: ["prettier", "vue"],
+  rules: {
+    "vue/component-definition-name-casing": ["warn", "kebab-case"],
+    "linebreak-style": "off",
+    "vue/script-setup-uses-vars": "error",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: false,
+        semi: true,
+        vueIndentScriptAndStyle: true,
+        endOfLine: "crlf",
+        bracketSameLine: true,
+      },
+    ],
+  },
+};
